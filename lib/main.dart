7a3c7591b7ce1761918ca85d0aaa3ce2f91f7881
shahrukhv5 <srkshahrukh735@gmail.com>
2025-01-21 10:43:17 +0530

    removed some useless code

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,3 @@
-// import 'package:flutter/material.dart';
-// import 'package:sigup_sigin_ui/Home.dart';
-// import 'package:sigup_sigin_ui/SplashScreen.dart';
-//
-// void main() {
-//   runApp(MaterialApp(
-//     debugShowCheckedModeBanner: false,
-//     initialRoute: 'splashScreen',
-//     routes: {
-//       'splashScreen': (context) => const Splashscreen(),
-//       'home': (context) => const Home(
-//             fullname: '',
-//             email: '',
-//             user: {},
-//           ),
-//     },
-//   ));
-// }
 import 'package:flutter/material.dart';
 import 'package:sigup_sigin_ui/SplashScreen.dart';
 
